ec5782b4b9b461c771c8bc622cd4968e9277c8c5
Lavender <susie204@naver.com>
2024-08-11 00:33:09 +0900

    Timer뷰 폴더, 빈파일

---
+++ LevelMaxDeFFain/LevelMaxDeFFain/Lavender/TimerView.swift
@@ -0,0 +1,18 @@
+//
+//  TimerView.swift
+//  LevelMaxDeFFain
+//
+//  Created by 이승현 on 8/10/24.
+//
+
+import SwiftUI
+
+struct TimerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TimerView()
+}
